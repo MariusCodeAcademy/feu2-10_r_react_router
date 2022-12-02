98a8ba005f6bc83a76e93cf4dfe815522c11ad73
Marius <marius@example.com>
2022-12-02 14:41:04 +0200

    image

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,10 @@ function Home(props) {
   return (
     <div>
       <h1>Home page</h1>
+      <img
+        src='https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124'
+        alt='image of react'
+      />
       <p className='subtitle'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium,
         suscipit.
