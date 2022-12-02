143cc595e0e77205522b8d1414ad9bc73dac799c
Marius <marius@example.com>
2022-12-02 13:57:25 +0200

    nav + pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <div className='App'>
+      <Nav />
       <h1>React</h1>
     </div>
   );
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+function Nav(props) {
+  return (
+    <nav>
+      <a href='/index.html'>Home page</a>
+      <a href='/about.html'>About page</a>
+      <a href='/contact.html'>Contact page</a>
+    </nav>
+  );
+}
+export default Nav;
